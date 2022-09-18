071754630292474251c7b512f28b97d311644ef1
Pablo <65398996+pablo-fdez-fdez@users.noreply.github.com>
2022-09-18 17:36:28 +0200

    version 1

--- logic.js
+++ logic.js
@@ -1,44 +1,38 @@
 window.addEventListener("load", function(){
+    // Prevent form from submitting on "enter" key pressed
+    document.getElementById("weatherForm").addEventListener("keypress", function(event){
+        if (event.key === "Enter"){
+            event.preventDefault();
+        }        
+    });
 
-    document.getElementById("search").addEventListener("click", async function(){
+    // Search button: print weather data of searched city
+    document.getElementById("search").addEventListener("click", async function(event){  
         let city = document.getElementById("location").value;
-        // Get Data from user introduced town
-        let data = await getWeatherData(city);
-            // Get data from kelvin to celsius system and round to 2 decimals
-        const TEMPERATURE = parseFloat((data['main'].temp) -  273.15).toFixed(2);        
-        const MAX_TEMPERATURE = parseFloat((data['main'].temp_max) -  273.15).toFixed(2);
-        const MIN_TEMPERATURE = parseFloat((data['main'].temp_min) -  273.15).toFixed(2);
+        // Get Data from user introduced town and display it, if not available display error
+        try {
+            let data = await getWeatherData(city);
+            // Get all data
+            const MAIN = data['weather'][0].main; // Main text: clouds, rain, etc
+            let description = data['weather'][0].description; // Description of main
+            description = description.charAt(0).toUpperCase() + description.slice(1);
+            const HUMIDITY = data['main'].humidity; // humidity percentage
+            const ICON = data['weather'][0].icon; // icon used to obtain the picture form openweathermap url
+            // Get temperature data and transform it from kelvin to celsius system and round to 2 decimals
+            const TEMPERATURE = parseFloat((data['main'].temp) -  273.15).toFixed(2);        
+            const MAX_TEMPERATURE = parseFloat((data['main'].temp_max) -  273.15).toFixed(2);
+            const MIN_TEMPERATURE = parseFloat((data['main'].temp_min) -  273.15).toFixed(2);
             // Display data in the screen
-        document.getElementById("weahter-results").innerHTML = displayData(city, TEMPERATURE, MAX_TEMPERATURE, MIN_TEMPERATURE);
+            document.getElementById("weahter-results").innerHTML = displayData(city, ICON, MAIN, description, MIN_TEMPERATURE, TEMPERATURE, MAX_TEMPERATURE, HUMIDITY);
+        } catch (error) {
+            document.getElementById("weahter-results").innerHTML = `<h3>'${city}' not found in database</h3>`;
+        }
+        
         // Reset form
         document.getElementById("location").value = "";
     })    
 
 });
-
-function displayData(city, temperature, max, min){
-    return `
-            <h2>${city.toUpperCase()}</h2>
-            <p><span><b>Min temperature: </b><span>${min}ºC</p>
-            <p><span><b>Average temperature: </b><span>${temperature}ºC</p>
-            <p><span><b>Max temperature: </b><span>${max}ºC</p>
-    `;
-}
-
-async function getWeatherData(city){
-    const API_KEY = "14150f4c93ebeebcf4a4101c2d5b9069"; // OpenWeather API user key
-    let limit = "1"; // Number of results returned per query (ex. multiple cities with similar names)
-
-    // Do query 1: returns longitude and latitude of desired city
-    let urlCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + limit + "&appid=" + API_KEY;
-    const LOCATION = await retrieveCoordinates(urlCoordinates);
-
-    // Do query 2: returns weather data of given city (requires query 1 to get latitude and longitude)
-    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?lat=" + LOCATION['latitude'] + "&lon=" + LOCATION['longitude'] + "&appid=" + API_KEY;       
-    let cityWeather = await retrieveWeatherData(urlWeather);
-
-    return cityWeather;
-}
 
 // Query to API 1: Use API to retrieve coordinates of a town (latitude and longitude)
 async function retrieveCoordinates(url){
@@ -74,4 +68,41 @@ async function retrieveWeatherData(url){
     return weahterData;
 }
 
+// Function to gather the data by calling APIs and then it returns its data
+async function getWeatherData(city){
+    const API_KEY = "5a590164a400ad0b07b8d70c8f5ef674"; // OpenWeather API user key
+    let limit = "1"; // Number of results returned per query (ex. multiple cities with similar names)
+
+    // Do query 1: returns longitude and latitude of desired city
+    let urlCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + limit + "&appid=" + API_KEY;
+    const LOCATION = await retrieveCoordinates(urlCoordinates);
+
+    // Do query 2: returns weather data of given city (requires query 1 to get latitude and longitude)
+    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?lat=" + LOCATION['latitude'] + "&lon=" + LOCATION['longitude'] + "&appid=" + API_KEY;       
+    let cityWeather = await retrieveWeatherData(urlWeather);
+
+    return cityWeather;
+}
+
+
+// Function to display all the data obtained from APIs --> display it in HTML elements
+function displayData(city, icon, main, description, min, temperature, max, humidity){
+    // Calculate number of pictures for humidity (1,2,3,4), according to percentage. Prepare the pictures HTML elements
+    const HUMIDITY_LEVEL = Math.ceil(parseInt(humidity)/25);
+    let humidityPictures ="";
+    for (let i = 0; i < HUMIDITY_LEVEL; i++){
+        humidityPictures += "<img src='humidity.png' class='humidity-picture'>";
+    }
+
+    return `
+            <h2>${city.toUpperCase()}</h2>
+            <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${main}" id='weather-icon'><br>
+            <p>${description}</p>
+            <p><span><b>Min temperature: </b><span>${min}ºC</p>
+            <p><span><b>Average temperature: </b><span>${temperature}ºC</p>
+            <p><span><b>Max temperature: </b><span>${max}ºC</p>
+            <p><span><b>Humidity: </b><span>${humidity}%</p>
+            <div id='humidity-container'>${humidityPictures}</div>
+    `;
+}
 
