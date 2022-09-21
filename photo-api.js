d1a2242ae17cd284304df8ff5a0589205efa109a
Pablo <110939260+pabloff95@users.noreply.github.com>
2022-09-21 22:50:14 +0200

    Update photo-api.js

--- photo-api.js
+++ photo-api.js
@@ -17,7 +17,7 @@ async function retrievePictureUrl(url){
 // ----------------------- FUNCTION TO OBTAIN DATA FROM QUERY TO API
 // Function to get the URL of a picture of a given town
 async function getPictureUrl(city){
-    const API_KEY = "0Jx0y2tQr8-I8slywV7H_nC6hhLPKir4mhygsmoW3As"; // Access key 
+    const API_KEY = ""; // [INTRODUCE] Access key 
     // Do query: returns URL of a picture from the desired city
     let photoURL = "https://api.unsplash.com/search/photos?query=" + city + "&page=1&per_page=1&client_id=" + API_KEY; // Big DB --> limit query to 1 photo
     let resultAPI = await retrievePictureUrl(photoURL);
